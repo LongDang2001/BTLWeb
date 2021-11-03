2f95b2748b70acb5cbd6f68e3c8af1b7bd1ca36d
rashf0rd <90589753+rashf0rd@users.noreply.github.com>
2021-11-03 22:53:05 +0700

    bản update 3.0

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+class Config
+{
+    const TITLE = 'Hệ Thống Trắc Nghiệm Online';
+    const CONTRIBUTOR = "";
+    const RELEASE = "15/05/2020";
+    const TIMEZONE = "Asia/Bangkok";
+    const APP_URL = "http://localhost/";
+}
